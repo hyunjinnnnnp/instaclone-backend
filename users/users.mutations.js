6f677daff568abd69d8ed4dff2bc72a1a707a29a
hyunjinnnnnp <imhjinnnnn@gmail.com>
2022-05-12 17:44:15 +0900

    [FEAT] Login mutation

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import client from "../client";
 
 export default {
@@ -36,6 +37,18 @@ export default {
       } catch (error) {
         return error;
       }
+    },
+    login: async (_, { username, password }) => {
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return { ok: false, error: "User not found." };
+      }
+      const passwordCorrect = await bcrypt.compare(password, user.password);
+      if (!passwordCorrect) {
+        return { ok: false, error: "Incorrect password." };
+      }
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return { ok: true, token };
     },
   },
 };
