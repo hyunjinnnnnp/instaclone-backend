27653e31fa5549a48b6e81a59ac1c392290faa87
hyunjinnnnnp <imhjinnnnn@gmail.com>
2022-05-12 19:17:19 +0900

    [REFACTOR] Split resolvers, typeDefs into smaller

--- schema.js
+++ schema.js
@@ -3,10 +3,7 @@ import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const loadedResolvers = loadFilesSync(
-  `${__dirname}/**/*.{queries,mutations}.js`
-);
-// .queries.js OR .mutations.js
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
 const typeDefs = mergeTypeDefs(loadedTypes);
 const resolvers = mergeResolvers(loadedResolvers);
 
