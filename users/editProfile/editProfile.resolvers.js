7c6c415d6b6be053251ac07093780b6aad9f5c1d
hyunjinnnnnp <imhjinnnnn@gmail.com>
2022-05-12 22:15:10 +0900

    [FEAT] Authentication: Protect resolvers - 2. Currying

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,36 +1,37 @@
 import bcrypt from "bcrypt";
 import client from "../../client";
+import { protectedResolver } from "../users.utils";
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, username, email, password: newPassword },
-      { loggedInUser, protectResolver }
-    ) => {
-      protectResolver(loggedInUser);
-      // 여기저기서 import 하지않기 위해 context에 protectResolver를 전달해서 사용한다
-      let hashedPassword = null;
-      if (newPassword) {
-        hashedPassword = await bcrypt.hash(newPassword, 10);
+    editProfile: protectedResolver(
+      async (
+        _,
+        { firstName, lastName, username, email, password: newPassword },
+        { loggedInUser }
+      ) => {
+        let hashedPassword = null;
+        if (newPassword) {
+          hashedPassword = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            ...(hashedPassword && { password: hashedPassword }),
+          },
+        });
+        if (updatedUser.id) {
+          return { ok: true };
+        } else {
+          return { ok: false, error: "Could not update profile." };
+        }
       }
-      const updatedUser = await client.user.update({
-        where: {
-          id: loggedInUser.id,
-        },
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          ...(hashedPassword && { password: hashedPassword }),
-        },
-      });
-      if (updatedUser.id) {
-        return { ok: true };
-      } else {
-        return { ok: false, error: "Could not update profile." };
-      }
-    },
+    ),
   },
 };
