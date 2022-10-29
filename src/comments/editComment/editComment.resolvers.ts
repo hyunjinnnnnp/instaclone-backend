c9cdb68bf242fd10d037b0af3ef27f2b11fe55cd
hyunjinnnnnp <imhjinnnnn@gmail.com>
2022-10-29 17:12:59 +0900

    editComment

---
+++ src/comments/editComment/editComment.resolvers.ts
@@ -0,0 +1,39 @@
+import { Resolver, Resolvers } from "../../types";
+import { protectedResolver } from "../../users/users.utils";
+
+const resolveFn: Resolver = async (
+  _,
+  { commentId, payload },
+  { client, loggedInUser }
+) => {
+  try {
+    const comment = await client.comment.findUnique({
+      where: { id: commentId },
+      select: { userId: true },
+    });
+    if (!comment) {
+      return { ok: false, error: "Comment not found." };
+    }
+    if (comment.userId !== loggedInUser.id) {
+      return { ok: false, error: "Not authorized." };
+    }
+    const ok = await client.comment.update({
+      where: { id: commentId },
+      data: {
+        payload,
+      },
+    });
+    console.log(ok);
+    return { ok: true };
+  } catch {
+    return { ok: false, error: "Could not edit." };
+  }
+};
+
+const resolvers: Resolvers = {
+  Mutation: {
+    editComment: protectedResolver(resolveFn),
+  },
+};
+
+export default resolvers;
